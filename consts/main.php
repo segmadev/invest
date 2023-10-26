f93358d7e372e6160c473b7a98e3ea0222ff3b81
Segma <71606399+serikioluwagbenga@users.noreply.github.com>
2023-10-26 20:36:52 +0100

    Update main.php

--- consts/main.php
+++ consts/main.php
@@ -3,8 +3,8 @@
     //  const db_password = "sJjJzBeJx2Qx";
     //  const db_name = "prolcnoz_db";
     //  const db_host_name = "localhost";
-     const db_username = "root";
-     const db_password = "";
-     const db_name = "invest";
-     const db_host_name = "localhost";
+     // const db_username = "root";
+     // const db_password = "";
+     // const db_name = "invest";
+     // const db_host_name = "localhost";
 ?>
