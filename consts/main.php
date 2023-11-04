d11ca485c84744c83c818237408e77c1f04a4044
net <71606399+serikioluwagbenga@users.noreply.github.com>
2023-11-04 15:26:17 +0100

    update

--- consts/main.php
+++ consts/main.php
@@ -3,9 +3,11 @@
      const db_password = "sJjJzBeJx2Qx";
      const db_name = "prolcnoz_db";
      const db_host_name = "localhost";
-     define("rootFile", str_replace("C:/xampp2/htdocs/invest2", "C:/xampp2/htdocs/invest2/app", $_SERVER['DOCUMENT_ROOT']."/invest2/"));
-     if (strpos(rootFile, "C:/xampp2/htdocs/") == false){
-        define("rootFile", str_replace("invest2", "", rootFile));
+     if (strpos($_SERVER['DOCUMENT_ROOT'], "C:/xampp2/htdocs/") == false){
+         define("rootFile", str_replace("C:/xampp2/htdocs/invest2", "C:/xampp2/htdocs/invest2/app", $_SERVER['DOCUMENT_ROOT']."/invest2/"));
+        //  echo "<h1>HERE</h1>";
+        }else{
+            define("rootFile", $_SERVER['DOCUMENT_ROOT']);
      }
 
 
