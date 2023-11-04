cb889ae91add312d1a2a1d70c0ec5cac3c388e5f
net <71606399+serikioluwagbenga@users.noreply.github.com>
2023-11-04 15:29:47 +0100

    update

--- consts/main.php
+++ consts/main.php
@@ -3,7 +3,7 @@
      const db_password = "sJjJzBeJx2Qx";
      const db_name = "prolcnoz_db";
      const db_host_name = "localhost";
-     if (strpos($_SERVER['DOCUMENT_ROOT'], "C:/xampp2/htdocs/") == false){
+     if (strpos($_SERVER['DOCUMENT_ROOT'], "htdocs") == false){
          define("rootFile", str_replace("C:/xampp2/htdocs/invest2", "C:/xampp2/htdocs/invest2/app", $_SERVER['DOCUMENT_ROOT']."/invest2/"));
          echo "<h1>HERE</h1>";
         }else{
