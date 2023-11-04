283a50b65b39dd7a3a4ba27245670bc117e72217
net <71606399+serikioluwagbenga@users.noreply.github.com>
2023-11-04 15:35:11 +0100

    update

--- consts/main.php
+++ consts/main.php
@@ -5,9 +5,9 @@
      const db_host_name = "localhost";
      if (strpos($_SERVER['DOCUMENT_ROOT'], "htdocs") == false){
          define("rootFile", $_SERVER['DOCUMENT_ROOT']."/app/");
-         echo "<h1>HERE</h1>";
+        //  echo "<h1>HERE</h1>";
         }else{
-            echo "<h1>HERE 2</h1>";
+            // echo "<h1>HERE 2</h1>";
             define("rootFile", str_replace("C:/xampp2/htdocs/invest2", "C:/xampp2/htdocs/invest2/app", $_SERVER['DOCUMENT_ROOT']."/invest2/"));
      }
 
