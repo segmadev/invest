c69d7b369023e2aef8a3dcf1e0cf45b831515e67
net <71606399+serikioluwagbenga@users.noreply.github.com>
2023-12-02 16:12:53 +0100

    fix bug

--- admin/pages/plans/ini.php
+++ admin/pages/plans/ini.php
@@ -1,5 +1,5 @@
 <?php
-$data = ["max_amount"=>0];
+$data = ["ID"=>uniqid(), "max_amount"=>0];
 if(isset($_GET['id'])) {
     $id = htmlspecialchars($_GET['id']);
     $data = $d->getall("plans", "ID = ?", [$id], fetch: "details");
