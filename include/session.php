a314d94027eafcec37360ccf41ee07100330a443
net <71606399+serikioluwagbenga@users.noreply.github.com>
2023-12-09 14:28:28 +0100

    show errr

--- include/session.php
+++ include/session.php
@@ -1,6 +1,7 @@
 <?php 
-// error_reporting(0);
-// ini_set('display_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // if($_SERVER[‘HTTPS’] != "on") {
  $redirect= "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 // header("Location:$redirect");
