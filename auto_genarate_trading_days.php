a8b363957055843fbb849c66a370516fe5c808de
net <71606399+serikioluwagbenga@users.noreply.github.com>
2023-12-12 10:19:50 +0100

    some work on investment

--- auto_genarate_trading_days.php
+++ auto_genarate_trading_days.php
@@ -7,8 +7,9 @@
     }
 
     if(isset($_GET['type']) && $_GET['type'] == "bot") {
-        $i->generate_trade_per_day();
-        // $i->auto_genarate_trading_days(htmlspecialchars($_GET['type']));   
+        // $i->generate_trade_per_day();
+        // auto_genarate_trading_days("bot", (int)$rand_no - (int)$check, $last_date)
+        $i->auto_genarate_trading_days(htmlspecialchars($_GET['type']), rand(200, 300), date("Y-m-d"));   
     }else{
         $i->auto_genarate_trading_days();   
     }
