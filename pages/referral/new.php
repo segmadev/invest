6a611ba97ca731c1dbf77db679b5c24821c1d887
net <71606399+serikioluwagbenga@users.noreply.github.com>
2023-12-18 18:58:11 +0100

    referral

--- pages/referral/new.php
+++ pages/referral/new.php
@@ -4,6 +4,7 @@
         if($u->activate_referral($userID, $referralID)){
             $code = htmlspecialchars($_POST['referral_code']);
             $refs = $d->getall("referrals", "referral_code = ?", [$code], fetch: "details");
+            echo "<b class='text-warning'>Please wait...</b>";
             $d->loadpage("index?p=referral&action=view&id=".$refs['ID']);
         }
     }
