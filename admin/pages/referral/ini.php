0ada30adc1bf7f0e3cf420634b6bf8cabef8aa18
net <71606399+serikioluwagbenga@users.noreply.github.com>
2023-12-18 18:48:37 +0100

    referral

--- admin/pages/referral/ini.php
+++ admin/pages/referral/ini.php
@@ -1,4 +1,15 @@
 <?php 
+$referral_from = [
+    "ID"=>["input_type"=>"number"],
+    "planID"=>["type"=>"select", "options"=>$d->options_list($d->getall("plans", fetch: "moredetails"), value: ["min_amount","max_amount","plan_name"])],
+    "plan_amount"=>["input_type"=>"number", "unique"=>"planID", "description"=>"Amount for plan invesment for the referral."],
+    "no_of_users"=>["input_type"=>"number", "unique"=>"planID"],
+    "percentage_return_on_deposit"=>["input_type"=>"number", "unique"=>"planID", 'description'=>'The (%) user will get on referral\'s first deposit.'],
+    "status"=>["placeholder"=>"Select Status","atb"=>"data-flag=\"ad\"", "options"=>["active"=>"Active", "disable"=>"Disable"],"type"=>"select"],
+    // "input_data"=>[]
+];
+$d->create_table("referral_programs", $referral_from);
+
 $ref_p = $d->getall("referral_programs", fetch: "moredetails");
 $ref_p_s = [];
 if(isset($_GET['id']) && $_GET['id'] != "") {
