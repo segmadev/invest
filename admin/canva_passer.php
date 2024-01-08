36aa3f61b359157c08b9049603d86bbd63288a4e
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-01-08 16:02:25 +0100

    chat

--- admin/canva_passer.php
+++ admin/canva_passer.php
@@ -1,5 +1,10 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once "../consts/main.php";
+if(!Regex){
+    define("Regex", "");
+}
 require_once "include/database.php";
 $d = new database;
 $screenshot_settings = [
