abcdabc9536e105a4fbaf4f9cdcafe0dca592f9c
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-01-08 16:05:23 +0100

    chat

--- admin/canva_passer.php
+++ admin/canva_passer.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once "../consts/main.php";
-if(!Regex){
+if (!defined('Regex')) {
     define("Regex", "");
-}
+} 
 require_once "include/database.php";
 $d = new database;
 $screenshot_settings = [
