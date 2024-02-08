e7d0bfde1ddbf115bb6095d4f27839d47129050c
net <71606399+serikioluwagbenga@users.noreply.github.com>
2024-02-08 13:55:28 +0100

    Fet: show no of messages send by a user in the admin

--- admin/pages/chat/options.php
+++ admin/pages/chat/options.php
@@ -18,11 +18,12 @@
         $message_form["time_sent"] = ['type'=>'input','input_type' => "datetime-local", "is_required" => false, "description" => '<small class="text-danger">You can leave it empty if you want to use current time. Only administrators can view a future message, but it will be automatically sent to users when the specified date arrives.</small>'];
         $message_form['senderID']['type'] = 'select';
         $users = [];
+        // var_dump($message_form['input_data']['receiverID']);
         if (isset($message_form['input_data']['receiverID'])) {
             $users_group = $ch->get_group_users_list($message_form['input_data']['receiverID'], 1, 100000, "first_name ASC");
             if ($users_group->rowCount() > 0) {
                 foreach ($users_group as $row) {
-                    $no_messages = $d->getall("message", "senderID = ? and receiverID = ? and time_sent < ?", [$row['user1'], $message_form['input_data']['receiverID'], time()], fetch: "");
+                    $no_messages = $d->getall("message", "senderID = ? and receiverID = ?", [$row['user1'], $message_form['input_data']['receiverID']], fetch: "");
                     $users[$row['user1']] = $ch->get_name($row['user1'], type: true)." - $no_messages Messages";
                 }
                 $message_form['senderID']['options'] = $users;
